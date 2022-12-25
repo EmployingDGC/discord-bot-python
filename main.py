7afb6215cad3bfea88a488e5522bc51f7356cf1a
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-12-25 18:28:11 -0300

    initial commit fork

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import bot
+#!/usr/bin/python3.11
 
-if __name__ == '__main__':
-    bot.run_discord_bot()
+import src.bot as bot
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv("./.env")
+
+
+if __name__ == "__main__":
+    TOKEN = os.environ.get("DISCORD_TOKEN")
+    
+    bot.run_discord_bot(TOKEN)
